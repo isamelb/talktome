4226da66065f118dc72691cbbd12c8729481d34d
isamelb <isamelb@gmail.com>
2019-07-09 12:44:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const btn = document.querySelector(".talk");
 const content = document.querySelector(".content");
 
-const motivate_me =["you surely can finish any project you start",
-                    "you should always love yourself",
-                    "life is beautifull, go out there and live",
-                    "create thing if you want to find happiness",
-                    "always repeat these words to yourself: I love myself, I love myself",
-                    "repeat this mantra to yourself, I can do anything I set myself to do"];
+
 
 const speechrecognition = window.speechrecognition || window.webkitSpeechRecognition;
 const recognition = new speechrecognition();
@@ -24,12 +19,17 @@ recognition.onresult = function(event){
 function readOutLoud(message){
 
 const speech = new SpeechSynthesisUtterance();
-speech.text = 'I dont know what you said, you can ask me to motivate you'
+speech.text = 'I dont know what you said, you can ask me to motivate you, or just say give me good affirmations'
 if (message.includes("motivate")){
    const finalText = motivate_me[Math.floor(Math.random()*motivate_me.length)];
    speech.text = finalText;
-
 }
+
+if (message.includes("affirmations")){
+    const finalText = positive_affirmations[Math.floor(Math.random()*positive_affirmations.length)];
+    speech.text = finalText;
+ }
+
 speech.volume = 1;
 window.speechSynthesis.speak(speech);
 }
---
+++ arrayChoices.js
@@ -0,0 +1,13 @@
+const motivate_me =["you surely can finish any project you start",
+                    "you should always love yourself",
+                    "life is beautifull, go out there and live",
+                    "create thing if you want to find happiness",
+                    "always repeat these words to yourself: I love myself, I love myself",
+                    "repeat this mantra to yourself, I can do anything I set myself to do"];
+
+const positive_affirmations=["you are a great person",
+                            "you are beautiful",
+                            "you are strong",
+                            "from now on your life will be filled with joy and happiness",
+                            "there no one like you, you are unique and you bring something special",
+                            "eveyone adores you, you are beautiful"];
