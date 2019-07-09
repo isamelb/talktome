e0973d1a19b702affddc33170fd17ca776631043
isamelb <isamelb@gmail.com>
2019-07-09 13:21:22 +0100

    Add files via upload

---
+++ arrayChoices.js
@@ -0,0 +1,14 @@
+const motivate_me =["you surely can finish any project you start",
+                    "you should always love yourself",
+                    "life is beautifull, go out there and live",
+                    "create thing if you want to find happiness",
+                    "always repeat these words to yourself: I love myself, I love myself",
+                    "repeat this mantra to yourself, I can do anything I set myself to do"];
+
+const positive_affirmations=["you are a great person, and you are proving it everyday, the more people know you the \
+                            more they admire and love you",
+                            "you are beautiful and you are going to succeed in life, life will always be pleasurable to you ",
+                            "you are strong, you can overcome any obstacles that try to prevent you from reaching your goals",
+                            "from now on your life will be filled with joy and happiness",
+                            "there is no one like you, you are unique and you bring something special",
+                            "everyone adores you, you are beautiful, you are doing the right thing in your life"];
